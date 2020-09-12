dab81de4d291855fdce458e7f72c32be38109bbb
Termux Professor <termuxprofessor@gmail.com>
2020-09-12 20:18:17 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -24,13 +24,13 @@ read -p " Does you want to Uninstall Parrot Shell?(Yes/No) : " input
 if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     clear
     cp default $HOME
+    cd $HOME
     mv default bash.bashrc
     cd /data/data/com.termux/files/usr/etc
     rm -rf bash.bashrc
     cd $HOME
-    cp bash.bashrc /data/data/com.termux/files/usr/etc
-    cd $HOME 
-    rm -rf default
+    mv bash.bashrc /data/data/com.termux/files/usr/etc
+    cd $HOME
     echo -e "\e[1;91mSuccessfully Uninstalled"
     echo -e "Restart Termux"
     exit  3
